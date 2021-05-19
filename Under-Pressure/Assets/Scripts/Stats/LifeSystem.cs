663a36aa2287804d48d8b773d5efd6797e8ef729
MateoVar <mvargasone@gmail.com>
2021-05-18 18:38:49 -0700

    Added in new heath system with diodes
    new health system still sort of a work in progress

---
+++ Under-Pressure/Assets/Scripts/Stats/LifeSystem.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifeSystem : MonoBehaviour
+{
+    public GameObject[] hearts;
+    private int life;
+    private bool dead;
+
+    private void Start()
+    {
+        life = hearts.Length;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (dead == true)
+        {
+            //insert death code here
+            Debug.Log("Player died...");
+        }
+        // can delete this when game is done
+        if (Input.GetKeyDown(KeyCode.T))
+        {
+            TakeDamage(1);
+        }
+    }
+
+    public void TakeDamage(int damage)
+    {
+        if (life >= 1)
+        {
+            life -= damage;
+            Destroy(hearts[life].gameObject);
+            if (life < 1)
+            {
+                dead = true;
+            }
+        }
+        
+    }
+}
